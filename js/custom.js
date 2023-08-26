11b29244a188536cf58dcfe8ac21215ab30a66ff
Buddhi Kavindra <visitbudkavin@gmail.com>
2023-08-26 21:15:31 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -9,10 +9,32 @@ getYear();
 
 /** google_map js **/
 
-function myMap() {
+/* function myMap() {
     var mapProp = {
         center: new google.maps.LatLng(40.712775, -74.005973),
         zoom: 18,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+} */
+
+/* Scroll to top */
+
+// Get the button:
+let mybutton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
